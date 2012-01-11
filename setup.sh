fe3391f68aecd5ceec59ac7d288cbf57bc05fa76
Nikola Knezevic <laladelausanne@gmail.com>
2012-01-11 15:51:35 +0100

    Fix: create dotfiles with setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ FILES='gitconfig gitattributes gitignore'
 SKELDIR=`pwd`
 
 cd ${HOME}
-for fname in ${FILES}; do ln -sf ${SKELDIR}/${fname} .; done
+for fname in ${FILES}; do ln -sf ${SKELDIR}/${fname} .${fname}; done
