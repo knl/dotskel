eed47692aa544b3295a8a9257271b87ed2181426
Nikola Knezevic <laladelausanne@gmail.com>
2015-05-08 22:01:33 +0200

    Switch to spacemacs, render dotemacs unnecessary

--- setup.sh
+++ setup.sh
@@ -6,12 +6,10 @@ FILES='
     gitignore \
     ackrc \
     tmux.conf \
+    spacemacs \
     '
 
 SKELDIR=`pwd`
-
-cd ${HOME}
-for fname in ${FILES}; do ln -sf ${SKELDIR}/${fname} .${fname}; done
 
 # setup keyremap4macbook
 mkdir -p ${HOME}/Library/Application\ Support/KeyRemap4MacBook/
@@ -20,3 +18,12 @@ ln -s ${SKELDIR}/keyremap4macbook/private.xml ${HOME}/Library/Application\ Suppo
 # setup quicksilver
 mkdir -p ${HOME}/Library/Application\ Support/Quicksilver
 ln -s ${SKELDIR}/quicksilver/Triggers.plist ${HOME}/Library/Application\ Support/Quicksilver/
+
+# spacemacs!
+# brew tap railwaycat/emacsmacport
+# brew install emacs-mac
+git clone --recursive http://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+
+# add config files at their right locations
+cd ${HOME}
+for fname in ${FILES}; do ln -sf ${SKELDIR}/${fname} .${fname}; done
