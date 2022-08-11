c2a140d1fae03f84106a2a4ffab1b9709cfe1842
Nikola Knezevic <nikola@knezevic.ch>
2022-08-11 23:06:44 +0200

    Remove unneded patches for emacs, after switching to 28.1

--- nix/overlays/emacsMacport.nix
+++ nix/overlays/emacsMacport.nix
@@ -6,23 +6,23 @@ self: super: rec {
   };
 
   emacsMacport = super.emacsMacport.overrideAttrs (old: rec {
-    patches = [
-      (super.fetchpatch {
-        name = "fix-window-role.patch";
-        url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/f3c16d68bbf52c1779be279579d124d726f0d04a/patches/emacs-27/fix-window-role.patch";
-        sha256 = "sha256:1hcfm6dxy2ji7q8fw502757920axffy32qlk9pcmpmk6q1zclgzv";
-      })
-      (super.fetchpatch {
-        name = "system-appearance.patch";
-        url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/f3c16d68bbf52c1779be279579d124d726f0d04a/patches/emacs-27/system-appearance.patch";
-        sha256 = "sha256:09zw6an5dxy1bjl3wx7wlrxhw6fp3i2qvdav83kqxlj06lz31miw";
-      })
-      # (super.fetchpatch {
-      #   name = "ligatures-freeze-fix.patch";
-      #   url = "https://github.com/d12frosted/homebrew-emacs-plus/raw/f3c16d68bbf52c1779be279579d124d726f0d04a/patches/emacs-27/ligatures-freeze-fix.patch";
-      #   sha256 = "sha256:0zldjs8nx26x7r8pwjc995lvpg06iv52rq4cy1w38hxhy7vp8lp3";
-      # })
-    ];
+    # patches = [
+    #   (super.fetchpatch {
+    #     name = "fix-window-role.patch";
+    #     url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/f3c16d68bbf52c1779be279579d124d726f0d04a/patches/emacs-27/fix-window-role.patch";
+    #     sha256 = "sha256:1hcfm6dxy2ji7q8fw502757920axffy32qlk9pcmpmk6q1zclgzv";
+    #   })
+    #   (super.fetchpatch {
+    #     name = "system-appearance.patch";
+    #     url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/f3c16d68bbf52c1779be279579d124d726f0d04a/patches/emacs-27/system-appearance.patch";
+    #     sha256 = "sha256:09zw6an5dxy1bjl3wx7wlrxhw6fp3i2qvdav83kqxlj06lz31miw";
+    #   })
+    #   # (super.fetchpatch {
+    #   #   name = "ligatures-freeze-fix.patch";
+    #   #   url = "https://github.com/d12frosted/homebrew-emacs-plus/raw/f3c16d68bbf52c1779be279579d124d726f0d04a/patches/emacs-27/ligatures-freeze-fix.patch";
+    #   #   sha256 = "sha256:0zldjs8nx26x7r8pwjc995lvpg06iv52rq4cy1w38hxhy7vp8lp3";
+    #   # })
+    # ];
     postPatch = old.postPatch + ''
       # copy the nice icon to it
       cp ${spacemacsIcon} mac/Emacs.app/Contents/Resources/Emacs.icns
