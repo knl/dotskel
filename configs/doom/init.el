d5d8f3cd9f737be060e1f1658d4271fb02a9c6b9
Nikola Knezevic <nikola.knezevic@imc.com>
2023-05-16 20:16:50 +0200

    Enable nix and yaml support in doomemacs

--- configs/doom/init.el
+++ configs/doom/init.el
@@ -152,7 +152,7 @@
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +roam2 +present)              ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
@@ -174,7 +174,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
