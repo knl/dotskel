4ec5f43599ea6c2e8a130d69b26a11e9ab80383e
Nikola Knezevic <nikola.knezevic@imc.com>
2023-09-19 06:50:59 +0200

    Switch to light theme for Emacs

--- configs/doom/config.el
+++ configs/doom/config.el
@@ -31,8 +31,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-monokai-pro)
-(setq doom-theme 'ujelly)
+(setq doom-theme 'doom-one-light)
+;; (setq doom-theme 'ujelly)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
