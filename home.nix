82591fb688f72197bae61a5a3f7a350f2d93ee6c
Nikola Knezevic <nikola.knezevic@imc.com>
2023-09-19 06:54:40 +0200

    Bump up state version

--- home.nix
+++ home.nix
@@ -837,6 +837,6 @@ rec {
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "21.03";
+    stateVersion = "23.05";
   };
 }
