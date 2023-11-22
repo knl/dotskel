c7b28f0334b1e9f19998c313615ddcc88dbab2a6
Nikola Knezevic <nikola@knezevic.ch>
2023-11-23 00:42:16 +0100

    Switch to Colemak is complete

--- configs/doom/config.el
+++ configs/doom/config.el
@@ -68,58 +68,6 @@
                "r" #'org-roam-node-find
                "R" #'org-roam-capture))
 
-;; (map! :mnv  "n"  #'evil-next-line
-;;       :nv   "N"  #'evil-join
-;;       :mnv  "gn" #'evil-next-visual-line
-;;       :nv   "gN" #'evil-join-whitespace
-;;       :mnv  "e"  #'evil-previous-line
-;;       :mnv  "E"  #'evil-lookup
-;;       :mnv  "ge" #'evil-previous-visual-line
-;;       ; :mnvo "i"  #'evil-forward-char
-;;       ; :mnv  "I"  #'evil-window-bottom
-;;       ; :nv   "gi" #'evil-lion-left
-;;       ; :nv   "gI" #'evil-lion-right
-;;       ; :mnv  "zi" #'evil-scroll-column-right
-;;       ; :mnv  "zI" #'evil-scroll-right
-;;       ; :mnv  "j"  #'evil-forward-word-end
-;;       ; :mnv  "J"  #'evil-forward-word-end
-;;       ; :mnv  "gj" #'evil-backward-word-end
-;;       ; :mnv  "gJ" #'evil-backward-word-end
-;;       :mnv  "k"  #'evil-search-next
-;;       :mnv  "K"  #'evil-search-previous
-;;       :mnv  "gk" #'evil-next-match
-;;       :mnv  "gK" #'evil-previous-match
-;;       ; :n    "l"  #'evil-undo
-;;       ; :v    "l"  #'evil-downcase
-;;       ; :v    "L"  #'evil-upcase
-;;       ; :nv   "gl" #'evil-downcase
-;;       ; :nv   "gL" #'evil-upcase
-;;       ; :n    "u"  #'evil-insert
-;;       ; :vo   "u"  evil-inner-text-objects-map
-;;       ; :n    "U"  #'evil-insert-line
-;;       ; :v    "U"  #'evil-insert
-;;       ; :n    "gu" #'evil-insert-resume
-;;       ; :n    "gU" #'evil-insert-0-line
-;;       ; (:map doom-leader-file-map
-;;       ;  "w"       #'save-buffer)
-;;       (:map evil-window-map
-;;        "n"       #'evil-window-down
-;;        "N"       #'+evil/window-move-down
-;;        "C-n"     #'evil-window-down
-;;        "C-S-n"   #'evil-window-move-very-bottom
-;;        "e"       #'evil-window-up
-;;        "E"       #'+evil/window-move-up
-;;        "C-e"     #'evil-window-up
-;;        "C-S-e"   #'evil-window-move-very-top
-;;        ; "i"       #'evil-window-right
-;;        ; "I"       #'+evil/window-move-right
-;;        ; "C-i"     #'evil-window-right
-;;        ; "C-S-i"   #'evil-window-move-far-right
-;;        "k"       #'evil-window-new)
-;;       (:map magit-mode-map
-;;        :nv "n"    #'evil-next-visual-line
-;;        :nv "e"    #'evil-previous-visual-line))
-
 ;;; :tools lsp
 ;; Disable invasive lsp-mode features
 (after! lsp-mode
@@ -204,13 +152,15 @@
 (setq doom-themes-treemacs-theme "doom-colors")
                                         ;(treemacs-load-theme "all-the-icons")
 
-;; (use-package! evil-colemak-basics
-;;   :after evil
-;;   :init
-;;   (setq evil-colemak-basics-layout-mod `mod-dh) ; Swap "h" and "m"
-;;   :config
-;;   (global-evil-colemak-basics-mode) ; Enable colemak rebinds
-;; )
+;; Until the related PR merged, I neeed to configure colemak binding manually
+;; https://github.com/hlissner/doom-emacs/issues/783
+(use-package! evil-colemak-basics
+  :after evil
+  :init
+  (setq evil-colemak-basics-layout-mod `mod-dh) ; Swap "h" and "m"
+  :config
+  (global-evil-colemak-basics-mode) ; Enable colemak rebinds
+)
 
 (use-package! atomic-chrome
   :defer 5                              ; since the entry of this
