835f2dbc5292951c25b3f996cb72b941d2c3d963
Nikola Knezevic <nikola.knezevic@imc.com>
2024-11-05 10:29:09 +0100

    Remove bad patch

--- home.nix
+++ home.nix
@@ -166,11 +166,6 @@ let
               url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
               sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
             })
-            # Use poll instead of select to get file descriptors
-            (pkgs.fetchpatch {
-              url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
-              sha256 = "sha256-jN9MlD8/ZrnLuP2/HUXXEVVd6A+aRZNYFdZF8ReJGfY=";
-            })
             # Enable rounded window with no decoration
             (pkgs.fetchpatch {
               url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
