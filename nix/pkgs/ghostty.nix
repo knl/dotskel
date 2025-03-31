23c5b36d75c27b7507303c1b386b6c6c9d530a45
Nikola Knezevic <nikola@knezevic.ch>
2025-03-31 16:53:17 +0200

    Update ghostty

--- nix/pkgs/ghostty.nix
+++ nix/pkgs/ghostty.nix
@@ -6,12 +6,12 @@
 }:
 stdenv.mkDerivation rec {
   pname = "ghostty";
-  version = "1.1.2";
+  version = "1.1.3";
 
   src = fetchurl {
     name = "${pname}-${version}.dmg";
     url = "https://release.files.ghostty.org/${version}/Ghostty.dmg";
-    hash = "sha256-1K0BOWg0ykR/pdCE6/b7XUSVcoD6ryLqRz6WBnUcSOE=";
+    hash = "sha256-ZOUUGI9UlZjxZtbctvjfKfMz6VTigXKikB6piKFPJkc=";
   };
 
   nativeBuildInputs = [ _7zz ];
