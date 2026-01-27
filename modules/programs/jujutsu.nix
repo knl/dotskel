dc03c352abce1956e6d1dee2291a4afab29b3438
Nikola Knezevic <nikola@knezevic.ch>
2026-01-27 09:51:20 +0100

    Introduce scripts, add some for jujutsu

--- modules/programs/jujutsu.nix
+++ modules/programs/jujutsu.nix
@@ -9,5 +9,11 @@ in
       enable = true;
     };
     xdg.configFile."jj/config.toml".source = link ../../configs/jj/config.toml;
+
+    home.activation.installScripts = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      for script in ${../../scripts}/{indent,jju,jjx}; do
+        ln -sf "$script" "$HOME/bin/$(basename "$script")"
+      done
+    '';
   };
 }
