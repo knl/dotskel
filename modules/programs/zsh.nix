a2a0e494851149dd5140fc06939850fb5b041319
Nikola Knezevic <nikola@knezevic.ch>
2026-03-14 12:18:00 +0100

    make sure we can run nix after upgrades

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -130,6 +130,11 @@ in
       initContent =
         let
           initExtraFirst = pkgs.lib.mkBefore ''
+	    # Make sure we can get nix after macos upgrade (when /etc/zshrc gets overwritten)
+	    if [[ ! $(command -v nix) && -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]]; then
+	        source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+	    fi
+
             DIRSTACKSIZE=10
 
             setopt   emacs
